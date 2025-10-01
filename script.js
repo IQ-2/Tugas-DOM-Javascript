5a9f9348ec3d80bc9343cb1b64fa5610c7476a95
IQBAL22 <muhamadiqbal9871@gmail.com>
2025-10-01 23:13:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const judul = document.getElementById('header-utama');
+judul.innerHTML = "<h1>Seleksi ID Berhasil</h1>";
+
+const list = document.getElementsByClassName('produk-item');
+Array.from(list).forEach(SemuaList => {
+    if (SemuaList.getAttribute('data-status') !== 'pending') {
+        SemuaList.innerText = '[UPDATED] ' + SemuaList.innerText;
+        SemuaList.classList.add('tebal');
+    }
+});
+
+const list2 = document.querySelector('div[data-status="pending"]');
+list2.classList.add('highlight');
+
+const ul = document.querySelectorAll('ul li');
+ul.forEach(li => {
+    li.innerHTML = '<span>Konten Baru dari JS</span>';
+});
